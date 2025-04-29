665880000b421e9d023e2c717be0b21b27668540
ViniciusBarros90 <vinicius_santoss@hotmail.com>
2025-04-28 22:43:59 -0300

    novos conceitos

---
+++ Conceitos/condicional-switch-case.js
@@ -0,0 +1,23 @@
+/**
+ * escolha (condicao):
+ * se x: faca
+ * se y: faca
+ * se z: faca.
+ 
+ */
+const peso =10
+
+switch(peso){
+    case 10:
+      console.log('P')
+      break
+    case 20:
+      console.log('M')
+      break
+    case 30:
+      console.log('G')
+      break
+    default:
+      console.log('ND')  
+
+}
